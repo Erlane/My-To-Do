d6aca37979fc9036178d4a7ef231258d937adcc6
Erlane Mendes <erlane.mendes31@gmail.com>
2020-07-30 16:44:34 -0300

    Updates

---
+++ precache-manifest.c0946a409a1a271e3b1e7bc2e31a1746.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc32e1592bec516be975a764aacb987",
+    "url": "/My-To-Do/index.html"
+  },
+  {
+    "revision": "e2259113651d49ff6e61",
+    "url": "/My-To-Do/static/js/2.858b91f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-To-Do/static/js/2.858b91f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaca526ec9d836ed2408",
+    "url": "/My-To-Do/static/js/main.1c3d0664.chunk.js"
+  },
+  {
+    "revision": "bba6e0d70785868069f8",
+    "url": "/My-To-Do/static/js/runtime-main.4539ced8.js"
+  },
+  {
+    "revision": "b17c864b42d4b7d7a2a5822b8c68774d",
+    "url": "/My-To-Do/static/media/arrow.b17c864b.png"
+  },
+  {
+    "revision": "67ec4847b520de6aa1fffe4241a1d54d",
+    "url": "/My-To-Do/static/media/fundo.67ec4847.png"
+  },
+  {
+    "revision": "dd0fc38c02c6b400d4ad714c902da980",
+    "url": "/My-To-Do/static/media/pencil.dd0fc38c.png"
+  }
+]);
